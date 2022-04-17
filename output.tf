e03fa9608d46fa8c6e82a675fb9101e41afb7c3d
admin <chakradhar06@gmail.com>
2022-04-16 21:01:57 -0400

    output module

--- output.tf
+++ output.tf
@@ -1,3 +1,9 @@
 output "URL" {
   value = "google.com"
 }
+
+# output prints a message on the screen
+# output block helps in printing the  created
+resources attributes and argument on the screen
+# outputs with modules works as a data transmitter
+# you can define multiple output blocks
