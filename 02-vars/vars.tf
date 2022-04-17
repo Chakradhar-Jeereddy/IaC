1881f7e6655edeef60c1dacb49d016eb9bc2a1fb
admin <chakradhar06@gmail.com>
2022-04-17 01:42:43 -0400

    output module

--- 02-vars/vars.tf
+++ 02-vars/vars.tf
@@ -1,7 +1,27 @@
-variable "sample" {
+variable "sample1" {
  default = "Hello World"
 }
 
-output "vars" {
+output "example1" {
   value =  var.sample
  }
+
+#terraform supports following variables
+# String, number, boolean -> string data should be quoted in double quotes, numbers and boolean need not.
+# Terraform doesn't support single quotes
+
+variable "sample2" {
+ default = 100
+}
+
+output "example2" {
+ value = var.sample2
+}
+
+variable "sample3" {
+ default = true
+}
+
+output "example3" {
+ value = var.sample3
+}
